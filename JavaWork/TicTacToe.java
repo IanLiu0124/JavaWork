bd192cd5d6ab9cb922ec8a73bb72dd89da7cf4dc
Ian <andyu0124@gmail.com>
2024-08-03 10:55:28 -0700

    Added start game button, and actionlistener for resetting the game

--- JavaWork/TicTacToe.java
+++ JavaWork/TicTacToe.java
@@ -2,15 +2,18 @@ import javax.swing.*;
 
 import java.awt.*;
 import java.awt.event.*;
-
-public class TicTacToe extends JFrame {
+public class TicTacToe extends JFrame implements ActionListener{
 
 	JFrame myFrame = new JFrame();
-	JButton myButtons[] = new JButton[9];
-	JPanel myPanel = new JPanel();
+	static JButton myButtons[] = new JButton[9];
+	static JPanel myPanel = new JPanel();
+	static JButton startButton = new JButton("Start");
+	static JLabel playerLabel;
+	static boolean player1Turn;
+	static boolean win;
 	public TicTacToe()
 	{
-		myFrame.setSize(900, 400);
+		myFrame.setSize(230, 350);
 		myFrame.setVisible(true);
 		myFrame.setLayout(new BorderLayout());
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,18 +21,42 @@ public class TicTacToe extends JFrame {
 		
 		for(int i=0 ; i<myButtons.length; i++)
 		{
-			myButtons[i] = new JButton(" Hello ");
-			myButtons[i].setSize(50, 50);
+			myButtons[i] = new JButton("");
+			myButtons[i].setPreferredSize(new Dimension(40, 40));
+			myButtons[i].addActionListener(this);
 			myPanel.add(myButtons[i]);
 		}
 		myFrame.add(myPanel, BorderLayout.NORTH);
+		startButton.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+		myFrame.add(startButton, BorderLayout.SOUTH);
 		myPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		
+		ActionListener myAction = (ActionEvent e)->
+		{
+			resetGame();
+		};
+	}
+	
+	public static void resetGame()
+	{
+		for (JButton button : myButtons) {
+            button.setText("");
+        }
+        player1Turn = true;
+        playerLabel.setText("Player X's Turn");
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TicTacToe game = new TicTacToe();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
