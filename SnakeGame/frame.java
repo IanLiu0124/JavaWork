7b12d493cf210d3774f633a651eb8ea567fe5d0e
Ian <gamrise@gmail.com>
2024-08-15 20:17:50 -0700

    started working on SnakeGame

---
+++ SnakeGame/frame.java
@@ -0,0 +1,11 @@
+
+public class frame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//snakeFrame myFrame = new snakeFrame();
+		new snakeFrame();
+		System.out.println("test");
+	}
+
+}
---
+++ SnakeGame/gamePanel.java
@@ -0,0 +1,51 @@
+import java.awt.Graphics;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class gamePanel extends JPanel{
+	
+	JButton myButton = new JButton();
+	gamePanel()
+	{
+		this.setSize(300,50);
+		myButton.setSize(40,60);
+		this.add(myButton);
+	}
+	
+	public void startGame()
+	{
+		
+	}
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+	public void draw(Graphics g)
+	{
+		
+	}
+	public void move()
+	{
+		
+	}
+	public void checkApple()
+	{
+		
+	}
+	public void checkCollisions()
+	{
+		
+	}
+	public void gameOver(Graphics g)
+	{
+		
+	}
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e)
+		{
+			
+		}
+	}
+	
+}
