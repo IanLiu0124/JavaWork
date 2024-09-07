cfda4f168ae71a3cc688479ea56c0bc004c10cca
Ian <gamrise@gmail.com>
2024-09-06 21:09:07 -0700

    Started working on the snake game. Game Panel frame complete with units drawn using paintComponent and draw

--- SnakeGame/gamePanel.java
+++ SnakeGame/gamePanel.java
@@ -1,26 +1,64 @@
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 import javax.swing.*;
 
-public class gamePanel extends JPanel{
+public class gamePanel extends JPanel implements ActionListener{
+	
+	static final int SCREEN_WIDTH = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT = 25;
+	static final int GAME_UNIT = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT;
+	static final int DELAY = 75;
+	int[] x = new int[GAME_UNIT];
+	int[] y = new int[GAME_UNIT];
+	int body = 7;
+	int score;
+	int appleX;
+	int appleY;
+	char direction = 'R';
+	boolean start = false;
+	Timer timer;
+	Random random;
+	
+	
 	
 	JButton myButton = new JButton();
 	gamePanel()
 	{
-		this.setSize(300,50);
-		myButton.setSize(40,60);
-		this.add(myButton);
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new  MyKeyAdapter());
+		startGame();
 	}
 	
 	public void startGame()
 	{
+		newApple();
+		start = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
+		
+		
 		
 	}
-	public void paintComponent(Graphics g)
+	public void paintComponent(Graphics g) 
+	//gets automatically called when the window is displayed
 	{
-		
+		super.paintComponent(g);
+		draw(g);
 	}
 	public void draw(Graphics g)
+	{
+		for (int i=0; i<SCREEN_HEIGHT / UNIT ; i++)
+		{
+			g.drawLine(i*UNIT, SCREEN_HEIGHT, i*UNIT, 0);
+			g.drawLine(0, i*UNIT, SCREEN_WIDTH, i*UNIT);
+		}
+	}
+	public void newApple()
 	{
 		
 	}
@@ -46,6 +84,11 @@ public class gamePanel extends JPanel{
 		{
 			
 		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
